d4e61d27c2c9e66d5d2f50a5a04ed7c59de4661b
Juan Pablo Uriol Balbín <juanubal@alumni.uv.es>
2017-11-09 15:42:52 +0100

    Práctica 4. Ej. 1 Fin

--- pr4/memcost1.cpp
+++ pr4/memcost1.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-const int NUMERO_EJECUCIONES = 100000;
+const int NUMERO_EJECUCIONES = 10000;
 const int N_ELEM = 10000;
 
 struct Prueba
@@ -12,15 +12,27 @@ struct Prueba
 };
 
 void costeMem1();
+void costeMem2();
 
 int main()
 {
+    double tiempo1, tiempo2, tiempo3, tiempo4;
     struct timespec ini_time, fin_time;
     clock_gettime(CLOCK_REALTIME, &ini_time);
     for(int i = 0; i < NUMERO_EJECUCIONES; i++)
         costeMem1();
     clock_gettime(CLOCK_REALTIME, &fin_time);
-    cout << "Tiempo: " << (double)fin_time.tv_sec - (double) ini_time.tv_sec + fin_time.tv_nsec * 1e-9 - ini_time.tv_nsec * 1e-9 << endl;
+    tiempo1 = (double)fin_time.tv_sec - (double) ini_time.tv_sec + fin_time.tv_nsec * 1e-9 - ini_time.tv_nsec * 1e-9;
+    cout << "Tiempo estático: " << tiempo1 << endl;
+    
+
+    clock_gettime(CLOCK_REALTIME, &ini_time);
+    for(int i = 0; i < NUMERO_EJECUCIONES; i++)
+        costeMem2();
+    clock_gettime(CLOCK_REALTIME, &fin_time);
+    tiempo2 = (double)fin_time.tv_sec - (double) ini_time.tv_sec + fin_time.tv_nsec * 1e-9 - ini_time.tv_nsec * 1e-9;
+    cout << "Tiempo dinámico: " << tiempo2 << endl;
+    cout << "Diferencia: " << tiempo2 - tiempo1 << endl;
 
     return 0;
 }
@@ -34,3 +46,20 @@ void costeMem1()
     for (int i = 0; i < N_ELEM; i++)
         suma += v[i].i1;
 }
+
+void costeMem2() 
+{
+    Prueba* v[N_ELEM];
+    volatile int suma = 0;
+    for (int i = 0; i < N_ELEM; i++)
+    {
+        v[i] = new Prueba;
+        v[i]->i1 = i;
+    }
+    for (int i = 0; i < N_ELEM; i++)
+    {
+        suma += v[i]->i1;
+        delete v[i];
+    }
+}
+    
