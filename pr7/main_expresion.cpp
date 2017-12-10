8fad3177da2808aee381f6356c57a5b84c6daa22
nagolli <nacho.gomis@gmail.com>
2017-12-10 16:21:17 +0100

    Parte 4 Hecha
    Faltan:
    Makefile
    Parte 2
    Parte 3
    Parte opcional
    Adecentar el main

--- pr7/main_expresion.cpp
+++ pr7/main_expresion.cpp
@@ -2,12 +2,16 @@
 
 #include<iostream>
 #include "expresion.h"
+//#include "sarray2.h"
+#include "Eigen/Dense"
 using namespace std;
+using Eigen::ArrayXd;
 
 const unsigned TAM = 1000000;
 
 int main()
 {
+    //PARTE 1
     Array<double> x(TAM), y(TAM);
 
     for(unsigned i = 0; i < TAM; i++)
@@ -23,4 +27,21 @@ int main()
         cout << x[i] << " ";
     }
     cout << endl;
+    //PARTE 2
+    
+    //PARTE 4
+    ArrayXd alfa(TAM);
+    ArrayXd beta(TAM);
+    for(unsigned i = 0; i < TAM; i++)
+    {
+        alfa[i] = 2;
+        beta[i] = 3;
+    }
+    
+    alfa =  (alfa+1.2)*beta + ((beta*2.0).pow(2.0));
+    
+    for(unsigned i = 0; i < 10; i++)
+    {
+        cout << alfa[i] << " ";
+    }
 }
